7b63e2aa91d24373943d7920ad0a2d2d4a658898
Aanchalg12 <119031281+Aanchalg12@users.noreply.github.com>
2024-04-14 22:24:49 +0100

    Update urls.py

--- Back-end/urls.py
+++ Back-end/urls.py
@@ -23,11 +23,18 @@ from .views import logout_view
 from .views import customerdashboard
 from django.conf import settings
 from django.conf.urls.static import static
-# from .views import soft_delete_user
+from .views import deleteuser
+from .views import application_details
+from .views import update_application
+from .views import delete_application
+# from .views import search_application
+from .views import generate_report
+from .views import search_results
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', user_login, name='login'),
+    path('', views.home, name='home'),
+    path('accounts/login/', user_login, name='login'),
     path('signup/', signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path("admindashboard", views.admindashboard, name="admindashboard"), 
@@ -39,12 +46,24 @@ urlpatterns = [
     path('user_list/', views.user_list, name='user_list'),
     path("adduser", views.adduser, name="adduser"),
     path("saveadduser", views.saveadduser, name="saveadduser"),
-    path('deleteuser/<int:user_id>/', views.deleteuser, name='deleteuser'),
     path("saveupdateuser", views.saveupdateuser, name="saveupdateuser"),
     path('application/', views.application, name='application'),
+    path("addapplication", views.addapplication, name="addapplication"),
     path('viewprofile/<int:user_id>/', views.viewprofile, name='viewprofile'),
-    path('search/', views.search_users, name='search_users'),
-   
+    # path('search/', views.search_users, name='search_users'),
+    path('search/', search_results, name='search_results'),
+    path('<int:pk>', application_details, name='application_details'),
+    path('<int:pk>/update/', update_application, name='update_application'),
+    path('<int:pk>/delete/', delete_application, name='delete_application'),
+    # path('search/', search_application, name='search_application'),
+    path('generate-report/<int:application_id>/', generate_report, name='generate_report'),
+    path('add-vendor/', views.vendor_details_add, name='vendor_details_add'),
+    path('vendor_details', views.vendor_details, name="vendor_details"),
+    path('vendor/<int:vendor_id>/', views.vendor_detail_info, name='vendor_detail_info'),
+    path('vendor_info/<int:vendor_info_id>/pdf/', views.generate_vendor_info_pdf, name='vendor_info_pdf'),
+    path('vendor_info/<int:vendor_info_id>/update/', views.update_vendor_info, name='update_vendor_info'),
+    path('vendor_info/<int:vendor_info_id>/delete/', views.delete_vendor_info, name='delete_vendor_info'),
+    path('deleteuser/<int:user_id>/', views.deleteuser, name='deleteuser'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
